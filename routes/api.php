c748aa3c477180534186ff94f28c21a5a79b077b
Cristian Camilo Echeverri <crisechevrri@gmail.com>
2025-09-29 18:59:33 -0500

    Métodos del controlador

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\VenueController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -17,4 +18,5 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
     Route::apiResource('events', EventController::class);
+    Route::apiResource('venues', VenueController::class);
 });
